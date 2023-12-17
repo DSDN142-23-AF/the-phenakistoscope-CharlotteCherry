29ea873e3f2a6cbd2332e5683e3f95aff1338658
CharlotteCherry <cherrychar@myvuw.ac.nz>
2023-12-17 14:56:07 +1300

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,10 +1,10 @@
 const SLICE_COUNT = 18;
 
 function setup_pScope(pScope){
-  pScope.output_mode(OUTPUT_GIF(1000));
+  pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false);
-  pScope.set_direction(CW);
+  pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
 }
 
@@ -13,13 +13,14 @@ function setup_layers(pScope){
   new PLayer(null, 246, 161, 178, 150);  //lets us draw the whole circle background, ignoring the boundaries
 
   var layer1 = new PLayer(Heart);
-  layer1.mode( SWIRL(5) );
+  layer1.mode( SWIRL(2) );
   layer1.set_boundary( 200, 800 );
 
-  //var layer2 = new PLayer(big heart);
-  //layer2.mode( RING );
-  //layer2.set_boundary( 0, 400 );
+  var layer2 = new PLayer(line);
+  layer2.mode(RING );
+  layer2.set_boundary( 0, 100);
 
+ 
   
 }
 
@@ -27,30 +28,25 @@ function Heart(x, y, animation, pScope){
   fill(223, 61, 108);
   stroke (223, 61, 108); 
 //scale(animation.frame*2);
-scale (1.5)
+//scale (animation.wave(1));
 
   ellipse(66.6,66.6,20,20); 
   ellipse (83.2,66.6,20,20);
   triangle (91.2,72.6,75,95,58.6,72.6);
   
+  line (100,50,0,50);
+scale (2);
+}
+
+function line(x, y, animation, pScope){
+  stroke (223, 61, 108); 
   
+//scale(animation.frame*1);
 
+line (1000,1000);
+strokeWeight (100)
 }
 
-function squares(x, y, animation, pScope){
-
-  // this is how you set up a background for a specific layer
-  let angleOffset = (360 / SLICE_COUNT) / 2
-  let backgroundArcStart = 270 - angleOffset;
-  let backgroundArcEnd = 270 + angleOffset;
-
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
-
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
-
-}
 
 
 
