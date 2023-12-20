3567b22256c30f444e74c27290d420898087682f
CharlotteCherry <cherrychar@myvuw.ac.nz>
2023-12-20 15:48:45 +1300

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -4,7 +4,7 @@ function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false);
-  pScope.set_direction(CCW);
+  pScope.set_direction(CW);
   pScope.set_slice_count(SLICE_COUNT);
 }
 
@@ -16,10 +16,7 @@ function setup_layers(pScope){
   layer1.mode( SWIRL(2) );
   layer1.set_boundary( 200, 800 );
 
-  var layer2 = new PLayer(line);
-  layer2.mode(RING );
-  layer2.set_boundary( 0, 100);
-
+  
  
   
 }
@@ -34,18 +31,9 @@ function Heart(x, y, animation, pScope){
   ellipse (83.2,66.6,20,20);
   triangle (91.2,72.6,75,95,58.6,72.6);
   
-  line (100,50,0,50);
-scale (2);
-}
-
-function line(x, y, animation, pScope){
-  stroke (223, 61, 108); 
   
-//scale(animation.frame*1);
-
-line (1000,1000);
-strokeWeight (100)
 }
+
 
 
 
