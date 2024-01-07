6def9996597dbac078f933f3e99450a7c572a490
CharlotteCherry <cherrychar@myvuw.ac.nz>
2024-01-07 16:58:33 +1300

    ee
    update

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,4 +1,4 @@
-const SLICE_COUNT = 18;
+const SLICE_COUNT = 14;
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
@@ -13,10 +13,12 @@ function setup_layers(pScope){
   new PLayer(null, 246, 161, 178, 150);  //lets us draw the whole circle background, ignoring the boundaries
 
   var layer1 = new PLayer(Heart);
-  layer1.mode( SWIRL(2) );
-  layer1.set_boundary( 200, 800 );
+  layer1.mode( SWIRL(4) );
+  layer1.set_boundary( 300, 1000 );
 
-  
+  var layer2 = new PLayer (middle);
+  layer2. mode ( RING );
+  layer2. set_boundary (0,100);
  
   
 }
@@ -24,19 +26,23 @@ function setup_layers(pScope){
 function Heart(x, y, animation, pScope){
   fill(223, 61, 108);
   stroke (223, 61, 108); 
-//scale(animation.frame*2);
-//scale (animation.wave(1));
+//scale(animation.frame*3);
+scale (animation.wave(2));
 
   ellipse(66.6,66.6,20,20); 
   ellipse (83.2,66.6,20,20);
   triangle (91.2,72.6,75,95,58.6,72.6);
-  
-  
+ 
 }
 
+function middle(x, y, animation, pScope){
+  fill(223, 61, 108);
+  stroke (223, 61, 108); 
+
+
+  
 
 
 
 
-
-
+}
