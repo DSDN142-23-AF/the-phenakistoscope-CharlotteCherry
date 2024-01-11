43bec6090471562119392692b0edcb9cfa1072a2
CharlotteCherry <cherrychar@myvuw.ac.nz>
2024-01-11 17:06:40 +1300

    update

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,48 +1,75 @@
-const SLICE_COUNT = 14;
+const SLICE_COUNT = 15;
 
 function setup_pScope(pScope){
-  pScope.output_mode(ANIMATED_DISK);
+    pScope.output_mode( ANIMATED_DISK  );
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false);
   pScope.set_direction(CW);
   pScope.set_slice_count(SLICE_COUNT);
+  pScope.load_image_sequence("ted" , "png",27);
+  pScope.load_image("bow","png")
+
+
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 246, 161, 178, 150);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 246, 161, 178, );  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(Heart);
-  layer1.mode( SWIRL(4) );
-  layer1.set_boundary( 300, 1000 );
 
-  var layer2 = new PLayer (middle);
-  layer2. mode ( RING );
-  layer2. set_boundary (0,100);
- 
+  var layer2 = new PLayer(ted);
+ layer2.mode(RING);
+ layer2.set_boundary( 500, 700 );
   
+ var layer1 = new PLayer(Heart2);
+ layer1.mode( SWIRL(2) );
+ layer1.set_boundary( 700, 8 );
+
+var layer4 = new PLayer (outsidering);
+layer4.mode(RING);
+layer4. set_boundary (970,1000);
+
+let CenterImage = new PLayer(bow);
+  CenterImage.mode(RING);
+  CenterImage.set_boundary(0,30);
+
+
 }
 
-function Heart(x, y, animation, pScope){
-  fill(223, 61, 108);
-  stroke (223, 61, 108); 
-//scale(animation.frame*3);
-scale (animation.wave(2));
 
+
+
+function Heart2(x, y, animation, pScope){
+  fill(0);
+  stroke (0);
+scale(animation.frame*2);
+//scale (animation.wave(1));
   ellipse(66.6,66.6,20,20); 
   ellipse (83.2,66.6,20,20);
-  triangle (91.2,72.6,75,95,58.6,72.6);
- 
-}
-
-function middle(x, y, animation, pScope){
-  fill(223, 61, 108);
-  stroke (223, 61, 108); 
+  triangle (91.2,72.6,75,95,58.6,72.6);}
 
 
+function ted(x, y, animation, pScope){
+  scale(2);
+  pScope.draw_image_from_sequence("ted" , 100, 300, animation.frame);
   
+}
 
 
-
+function outsidering(x, y, animation, pScope){
+ 
+  pScope.fill_background(233,61,108);
 
 }
+
+function bow (x,y,animation,pScope){
+  scale(2);
+  pScope.draw_image("bow",x,y);
+}
+
+
+
+
+
+
+
